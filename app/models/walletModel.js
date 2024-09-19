741e81a5e962e1b6461add5125dd99e2e8d14f5f
sanarajan <sanamol87@gmail.com>
2024-09-19 23:51:17 +0530

    add referel offer while user registration

--- app/models/walletModel.js
+++ app/models/walletModel.js
@@ -6,11 +6,13 @@ const walletSchema = new mongoose.Schema(
    
         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 
-      Balance: {type:Number,required:true},  
+      Balance: {type:Number,default:0},  
       History: [ 
         {
-          TransactionType:  { type: String, required: true, unique: true },  // Type of transaction (e.g., credit, debit)
-          Amount:  {type:Number,required:true},  // Amount of the transaction
+          TransactionType:  { type: String,
+            enum: ['credit', 'debit', 'Referral', 'purchase', 'Refund'],
+          },  // Type of transaction (e.g., credit, debit)
+          Amount:  {type:Number,default:0},  // Amount of the transaction
           Date: { type: Date, default: null} 
         }
       ]
--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,16 +1,13 @@
 // middleware/isUserAuthenticated.js
 function isUserAuthenticated(req, res, next) {
-  console.log('Checking authentication');
+  console.log("Checking authentication");
 
-    if (req.session && req.session.useremail&&req.session.userType===2) {
-      next();
-    } else {
-      // User is not authenticated, redirect to login page
-      res.redirect('/');
-    }
+  if (req.session && req.session.useremail && req.session.userType === 2) {
+    next();
+  } else {
+    // User is not authenticated, redirect to login page
+    res.redirect("/");
   }
+}
 
-
-  
-  module.exports = isUserAuthenticated;
-  
+module.exports = isUserAuthenticated;
